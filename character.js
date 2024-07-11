c7f1ebb208cc85fc40eabf1f4de6a9c14bea7304
Ariel Park <spar0426@uni.sydney.edu.au>
2024-07-11 19:35:09 +1000

    added

---
+++ character.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ctcButton = document.getElementById('ctc');
+    var stgButton = document.getElementById('stg');
+
+    ctcButton.onclick = function() {
+        window.location.href ='chris1.html';
+    }
+    stgButton.onclick = function() {
+        window.location.href = 'shun1.html';
+    }
+});
---
+++ home.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var button = document.getElementById('startButton');
+
+    button.onclick = function() {
+        window.location.href = 'character.html'
+    }
+});
